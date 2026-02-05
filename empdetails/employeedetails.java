754c61c073f474eb9b8bc1490a00805ffc937210
purushoth2598 <purushothsfdc@gmail.com>
2026-02-05 20:51:17 +0530

    Add files via upload
    day1

---
+++ empdetails/employeedetails.java
@@ -0,0 +1,27 @@
+package work1.empdetails;
+
+public class employeedetails {
+
+	public static void main(String[] args) {
+		 String name = "John Doe";
+		int 	age = 30;
+		double  salary = 55000.75;
+		 char gender = 'M';
+		boolean acceptTerms = true;
+		float rating = 4.5f;
+		long mobileNumber = 9876543210L;
+		int pinCode = 56001;
+		int luckyNumber = 7;
+		System.out.println("Employeename -"+ name);
+		System.out.println("Employee age -"+ age);
+		System.out.println("Employee sal -"+ salary);
+		System.out.println("Employee gender -"+ gender);
+		System.out.println("EmployeeacceptTerms -"+ acceptTerms);
+		System.out.println("Employeerating - "+ rating);
+		System.out.println("Employee no  -"+ mobileNumber);
+		System.out.println("Employee pin  -"+ pinCode);
+		System.out.println("Employee no -"+ luckyNumber);
+		
+	}
+
+}

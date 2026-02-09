e2536f79cb9e3da7435c5d10b682ebeb91c5d455
purushoth2598 <purushothsfdc@gmail.com>
2026-02-09 23:02:35 +0530

    Add files via upload
    day 2 task

---
+++ fibanocci.java
@@ -0,0 +1,17 @@
+package Day2;
+
+public class fibanocci {
+
+	public static void main(String[] args) {
+		int total=8;
+		int a =0 ,b=1;
+		for (int i=0;i<total;i++) {
+			System.out.println(a +"");
+			int y = a+b;
+			 a=b;
+			 b= y;
+		
+		            
+		        }
+		    }
+		}
